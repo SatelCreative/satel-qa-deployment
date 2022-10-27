81582ee727168ae18016f718a5ab6b95237e1002
Rahul <rahulpatidar0191@gmail.com>
2022-10-26 19:54:51 -0700

    first commit

---
+++ dist/deploy_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+APP_NAME=$1
+DOCKER_USER=$2
+DOCKER_PASS=$3
+REGISTRY=$4
+
+echo "Deploy to ${APP_NAME}-qa.satelapps.com" 
+export DOCKER_TLS_VERIFY='1' 
+export DOCKER_HOST='tcp://34.234.172.171:2376'
+export DOCKER_CERT_PATH='/home/satel/.docker/machine/machines/satel-webapps-qa'
+docker login --username=$DOCKER_USER --password=$DOCKER_PASS $REGISTRY
+docker stack deploy --with-registry-auth -c docker-compose.qa.yml ${APP_NAME}
